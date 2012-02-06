9f34ff77ad56f244a602393588b96c62301ba5c5
Darii Denis <denis.darii@gmail.com>
2012-02-06 15:07:24 +0100

    X. Initial project skeleton

---
+++ pg_fabrep/__init__.py
@@ -0,0 +1,17 @@
+VERSION = (0, 0, 1, 'alpha', 1)
+
+
+def get_version(version=None):
+    """Derives a PEP386-compliant version number from VERSION."""
+    if version is None:
+        version = VERSION
+    assert len(version) == 5
+    assert version[3] in ('alpha', 'beta', 'rc', 'final')
+
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    return '.'.join(str(x) for x in version[:parts])

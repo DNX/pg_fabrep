34aec78e2c6a78df594c9519d13000c663a51549
DNX <none@none>
2012-03-08 11:57:01 +0100

    added repmgr_deb parameter and tests

--- pg_fabrep/tasks.py
+++ pg_fabrep/tasks.py
@@ -8,7 +8,7 @@ from fabric.api import env, puts, abort, task
 from fabric.contrib import console
 #from fabric.contrib.files import upload_template
 
-from fabric.colors import _wrap_with, green
+from fabric.colors import _wrap_with, green, red
 
 green_bg = _wrap_with('42')
 red_bg = _wrap_with('41')
@@ -45,9 +45,15 @@ def setup():
 @task
 def test_configuration():
     errors = []
+    if not env.get('configured'):
+        puts(red('Configuration missing! Please read README.rst first or go ahead at your own risk.'))
+        return False
 
+    supported_postgres_versions = ("9.0", "9.1")
     if not env.postgres_version:
         errors.append('Postgres version missing')
+    elif env.postgres_version not in supported_postgres_versions:
+        errors.append('Wrong postgres version, supported versions are: "%s"' % '", "'.join(supported_postgres_versions))
     if not env.cluster_name:
         errors.append('Cluster name missing')
     elif not re.search(r"^\w+$", env.cluster_name):
@@ -79,10 +85,12 @@ def test_configuration():
         errors.append('Sync user missing')
     if not env.get('sync_pass'):
         errors.append('Sync password missing')
+    if not env.get('repmgr_deb'):
+        errors.append('Repmgr deb file path missing')
 
     # print some feedback
     if errors:
-        puts('Configuration test revealed %i errors:' % len(errors))
+        puts(red('Configuration test revealed %i errors:' % len(errors)))
         puts('%s\n\n* %s\n' % ('-' * 37, '\n* '.join(errors)))
         puts('-' * 40)
         puts('Please fix them or go ahead at your own risk.')
@@ -95,25 +103,26 @@ def test_configuration():
 @task
 def print_configuration():
     parameters_info = []
-    parameters_info.append(('Postgres version', env.postgres_version))
-    parameters_info.append(('Cluster name', env.cluster_name))
-    parameters_info.append(('Cluster port', env.cluster_port))
-    parameters_info.append(('--- Master Configuration', '----------'))
-    parameters_info.append(('Master server IP', env.pgmaster_ip))
-    parameters_info.append(('Master node number', env.master_node_number))
-    parameters_info.append(('Master user@host', env.pgmaster_user_host))
-    parameters_info.append(('Master pgconf path', env.master_pgconf_path))
-    parameters_info.append(('Master pgdata path', env.master_pgdata_path))
-    parameters_info.append(('--- Slave Configuration', '----------'))
-    parameters_info.append(('Slave server IP', env.pgslave_ip))
-    parameters_info.append(('Slave node number', env.slave_node_number))
-    parameters_info.append(('Slave user@host', env.pgslave_user_host))
-    parameters_info.append(('Slave pgconf path', env.slave_pgconf_path))
-    parameters_info.append(('Slave pgdata path', env.slave_pgdata_path))
-    parameters_info.append(('--- Repmgr Configuration', '----------'))
-    parameters_info.append(('Sync database name', env.sync_db))
-    parameters_info.append(('Sync user', env.sync_user))
-    parameters_info.append(('Sync password', env.sync_pass))
+    parameters_info.append(("Postgres version", env.postgres_version))
+    parameters_info.append(("Cluster name", env.cluster_name))
+    parameters_info.append(("Cluster port", env.cluster_port))
+    parameters_info.append(("--- Master Configuration", "----------"))
+    parameters_info.append(("Master server IP", env.pgmaster_ip))
+    parameters_info.append(("Master node number", env.master_node_number))
+    parameters_info.append(("Master user@host", env.pgmaster_user_host))
+    parameters_info.append(("Master pgconf path", env.master_pgconf_path))
+    parameters_info.append(("Master pgdata path", env.master_pgdata_path))
+    parameters_info.append(("--- Slave Configuration", "----------"))
+    parameters_info.append(("Slave server IP", env.pgslave_ip))
+    parameters_info.append(("Slave node number", env.slave_node_number))
+    parameters_info.append(("Slave user@host", env.pgslave_user_host))
+    parameters_info.append(("Slave pgconf path", env.slave_pgconf_path))
+    parameters_info.append(("Slave pgdata path", env.slave_pgdata_path))
+    parameters_info.append(("--- Repmgr Configuration", "----------"))
+    parameters_info.append(("Sync database name", env.sync_db))
+    parameters_info.append(("Sync user", env.sync_user))
+    parameters_info.append(("Sync password", env.sync_pass))
+    parameters_info.append(("Repmgr deb file path", env.repmgr_deb))
 
     # print collected info
     puts('========== START YOUR CONFIGURATION ============')
@@ -133,6 +142,8 @@ def parameter_default_values():
         env.cluster_port = 5432
     if 'ask_confirmation' not in env:
         env.ask_confirmation = True
+    if 'pgmaster_ip' not in env:
+        env.pgmaster_ip = ''
     if 'master_node_number' not in env:
         env.master_node_number = 1
     if 'pgmaster_user_host' not in env:
@@ -141,6 +152,8 @@ def parameter_default_values():
         env.master_pgconf_path = '/etc/postgresql/%s/%s/' % (env.postgres_version, env.cluster_name)
     if 'master_pgdata_path' not in env:
         env.master_pgdata_path = '/var/lib/postgresql/%s/%s/' % (env.postgres_version, env.cluster_name)
+    if 'pgslave_ip' not in env:
+        env.pgslave_ip = ''
     if 'slave_node_number' not in env:
         env.slave_node_number = 2
     if 'pgslave_user_host' not in env:
@@ -155,3 +168,5 @@ def parameter_default_values():
         env.sync_user = 'syncuser'
     if 'sync_pass' not in env:
         env.sync_pass = 'syncpass'
+    if 'repmgr_deb' not in env:
+        env.repmgr_deb = "postgresql-repmgr-%s_1.0.0.deb" % env.postgres_version
